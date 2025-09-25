4a7ea9fe29bffe445c0fca08e5e95d1c2c9b9e6f
Chemical-dumbneer <jeanpiresdecarlos@gmail.com>
2025-09-25 19:10:09 -0300

    modificado a estrutura do projeto + arquivos dummy para demais telas

---
+++ controls/index.php
@@ -0,0 +1,14 @@
+<?php
+// exemplo: simulação de tickets vindos do banco
+$tickets = [
+    [
+        'usuario' => 'Fulano da Silva',
+        'titulo' => 'Meu PC não liga',
+        'descricao' => 'Lorem ipsum depois que o encanador trocou o cano o pc desligou'
+    ],
+    [
+        'usuario' => 'Maria Oliveira',
+        'titulo' => 'Erro ao acessar sistema',
+        'descricao' => 'Sistema retorna tela branca ao logar.'
+    ]
+];
