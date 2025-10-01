1ea96cd66366ea660eb5449b60c642f6b920d10f
Chemical-dumbneer <jeanpiresdecarlos@gmail.com>
2025-10-01 19:32:23 -0300

    correção de variáveis. implementado o composer. cabeçalho agora permite sair da sessão

--- public/logout.php
+++ public/logout.php
@@ -3,10 +3,8 @@ declare(strict_types=1);
 
 session_start();
 
-// Destrói todas as variáveis de sessão
 $_SESSION = [];
 
-// Se quiser, remove o cookie de sessão também
 if (ini_get("session.use_cookies")) {
     $params = session_get_cookie_params();
     setcookie(session_name(), '', time() - 42000,
@@ -15,9 +13,7 @@ if (ini_get("session.use_cookies")) {
     );
 }
 
-// Finalmente, destrói a sessão
 session_destroy();
 
-// Redireciona para a tela de login
 header("Location: /index.php");
 exit;
