54a33fa58f14bbf2a54f41fd711e95150dc25c36
Chemical-dumbneer <jeanpiresdecarlos@gmail.com>
2025-10-01 19:32:48 -0300

    correção de variáveis. implementado o composer. cabeçalho agora permite sair da sessão

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'utfpr/projeto-webservice',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'f3e2222f74d4297a6387feaf47945e61350feeae',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'utfpr/projeto-webservice' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'f3e2222f74d4297a6387feaf47945e61350feeae',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
